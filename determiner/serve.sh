984694f7c05d0ab2fd21f38554f22d43debc0ce5
alsruf36 <alsruf36@gmail.com>
2022-08-13 22:57:00 +0900

    added

--- determiner/serve.sh
+++ determiner/serve.sh
@@ -1,8 +1,8 @@
-python3 bertEveluator.py    --ray_address 0.0.0.0 \
+python3 bertEveluator.py    --ray_address 10.106.141.250 \
                             --ray_port 10001 \
                             --file_name "\"kobert/model-1654515965.pt\"" \
-                            --s3_end_point 0.0.0.0 \
+                            --s3_end_point 10.101.6.249 \
                             --s3_port 9000 \
                             --s3_access_key admin \
-                            --s3_secret_key pass \
+                            --s3_secret_key KSHS-1st-PANDAS \
                             --api_name analyze
