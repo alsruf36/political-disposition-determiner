8da5dd9188ba92c128b7089b5527c2a0cdec0426
alsruf36 <alsruf36@gmail.com>
2022-08-17 21:42:45 +0900

    updated

---
+++ determiner/scripts/loadEnv.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='loadEnv')
+parser.add_argument('-f', '--file', default='.env', type=str)
+args = parser.parse_args()
+
+with open("/tmp/" + args.file, 'r') as f:
+    result = dict(tuple(line.replace('\n', '').split('=')) for line in f.readlines() if not line.startswith('#'))
+
+with open("/airflow/xcom/return.json", "w") as file:
+    json.dump(serve_list, file)
--- determiner/serve.sh
+++ determiner/serve.sh
@@ -3,11 +3,11 @@ source <(cat .env | sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1
 RAY_ADDRESS=kubectl get svc -A | awk '/ray-ray-head/{print $4}'
 S3_ADDRESS=kubectl get svc -A | awk '/minio/{print $4}'
 
-python3 bertEveluator.py    --ray_address ${RAY_ADDRESS} \
-                            --ray_port 10001 \
-                            --file_name "\"kobert/model-1654515965.pt\"" \
-                            --s3_end_point ${S3_ADDRESS} \
-                            --s3_port 9000 \
-                            --s3_access_key ${S3_ACCESS_KEY} \
-                            --s3_secret_key ${S3_SECRET_KEY} \
-                            --api_name analyze
+python3 scripts/bertEveluator.py    --ray_address ${RAY_ADDRESS} \
+                                    --ray_port 10001 \
+                                    --file_name "\"kobert/model-1654515965.pt\"" \
+                                    --s3_end_point ${S3_ADDRESS} \
+                                    --s3_port 9000 \
+                                    --s3_access_key ${S3_ACCESS_KEY} \
+                                    --s3_secret_key ${S3_SECRET_KEY} \
+                                    --api_name analyze
