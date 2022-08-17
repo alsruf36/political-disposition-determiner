7bd368c74befea9ba1fbefdf14ee7dfcc51da52b
alsruf36 <alsruf36@gmail.com>
2022-08-17 16:16:12 +0900

    updated

--- determiner/serve.sh
+++ determiner/serve.sh
@@ -1,8 +1,13 @@
-python3 bertEveluator.py    --ray_address 10.106.141.250 \
+source <(cat .env | sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g")
+
+RAY_ADDRESS=kubectl get svc -A | awk '/ray-ray-head/{print $4}'
+S3_ADDRESS=kubectl get svc -A | awk '/minio/{print $4}'
+
+python3 bertEveluator.py    --ray_address ${RAY_ADDRESS} \
                             --ray_port 10001 \
                             --file_name "\"kobert/model-1654515965.pt\"" \
-                            --s3_end_point 10.101.6.249 \
+                            --s3_end_point ${S3_ADDRESS} \
                             --s3_port 9000 \
-                            --s3_access_key admin \
-                            --s3_secret_key KSHS-1st-PANDAS \
+                            --s3_access_key ${S3_ACCESS_KEY} \
+                            --s3_secret_key ${S3_SECRET_KEY} \
                             --api_name analyze
