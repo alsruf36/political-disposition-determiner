cd8972ef86cd6aebae4001e931d94c1a8311e68e
alsruf36 <alsruf36@gmail.com>
2022-10-26 03:19:39 +0900

    added

--- determiner/community.sh
+++ determiner/community.sh
@@ -1,3 +1,4 @@
+source ../.venv/bin/activate
 source <(cat .env | sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g")
 
 RAY_ADDRESS=`kubectl get svc -A | awk '/raycluster-kube-head-svc/{print $4}'`
